2d6d65ce5c5e180b9db8a9a19d61ce4e683566ac
Jan Fiedler <jan.fiedler@de.amadeus.com>
2014-09-04 07:38:14 +0200

    some minor tweaks

---
+++ fast.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void);
+
+void printSpeed(int speed);
+
+// read all the stuff from the pipe
+char *inputString(FILE* fp, size_t size);
+
+// which letter to paint red
+int pivotLetter(int l);
+
+// strpbrk backwards, data marks minimal index
+char *backwards(char *data, char *index, char *accept);
+
+// fast read looper
+void fastread(char *data, int x, int y, int speed);
+
